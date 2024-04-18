7dd7dac25002c52b88a662c5ec024e5f8caaa2fe
Pedro Souza <phmartinsdesouza@gmail.com>
2024-04-18 19:49:40 -0300

    improving every time in the FE

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }, skip: [:registrations]
+  devise_scope :user do
+    get 'users/sign_up', to: 'users/registrations#new', as: :new_user_registration
+    post 'users', to: 'users/registrations#create', as: :user_registration
+    get 'users/edit', to: 'users/registrations#edit', as: :edit_user_registration
+    put 'users', to: 'users/registrations#update'
+    patch 'users', to: 'users/registrations#update'
+    delete 'users', to: 'users/registrations#destroy'
+  end
   root to: "posts#index"
   resources :comments, except: %i[ :show ]
   resources :posts do
