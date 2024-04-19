cd23a6c8ba8194234429a6ff125bfcf7fc453991
Pedro Souza <phmartinsdesouza@gmail.com>
2024-04-19 17:50:30 -0300

    modal almost done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,11 +37,11 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-   # Don't care if the mailer can't send.
-   config.action_mailer.raise_delivery_errors = false
-   # I'm using MailCatcher to catch emails in development
-   config.action_mailer.delivery_method = :smtp
-   config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+  # I'm using MailCatcher to catch emails in development
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
 
   config.action_mailer.perform_caching = false
 
